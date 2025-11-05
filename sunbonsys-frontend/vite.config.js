714ea776992884a9e4e6df2b8cfbf2c0a88d7b00
Aditya Bonde <admitbonde123@gmail.com>
2025-11-05 14:53:11 +0530

    Fix

--- sunbonsys-frontend/vite.config.js
+++ sunbonsys-frontend/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),   // ✅ THIS IS THE FIX
+    tailwindcss(),
   ],
+  base: process.env.VITE_BASE_PATH || "sunbonsys"
 })
